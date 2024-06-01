fa2b0ed045db13c67a08f7f54e65cd188ee7e1b8
techiesandeep <65929239+techiesandeep@users.noreply.github.com>
2024-06-01 01:56:04 -0500

    Thymeleaf excercise

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,7 @@
 package org.launchcode.codingevents.controllers;
 
+import jdk.jfr.Description;
+import jdk.jfr.Name;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,17 +9,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-        private static  List<String> listOfEvents = new ArrayList<>();
+        private static  HashMap<Name,Description> events = new HashMap<>();
+         events.put("Menteaship","A fun meetup for connecting with mentors");
+         events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+         events.put("Javascripty", "An imaginary meetup for Javascript developers");
     @GetMapping
     public String eventList(Model model){
-        model.addAttribute("listOfEvents",listOfEvents );
+        model.addAttribute("listOfEvents",events );
         return "events/index";
 
     }
@@ -31,7 +36,7 @@ public class EventController {
 //lives at path /events/form
     @PostMapping("form")
     public String createEventForFormSubmission(@RequestParam String eventName){
-        listOfEvents.add(eventName);
+        events.put(eventName);
         return "redirect:/events";
     }
 }
